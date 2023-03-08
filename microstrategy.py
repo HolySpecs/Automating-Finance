be939e594d959282a946f683b9230c7335ccd5ce
Nicholas Henryanto <n.henryanto2510@gmail.com>
2023-03-08 16:08:25 +0100

    added wait for download function and future notes

--- microstrategy.py
+++ microstrategy.py
@@ -23,6 +23,17 @@ def waitForElement(driver, howToLocateElement, element):
 def executeScript(driver, element):
     driver.execute_script("arguments[0].click();", waitForElement(driver, By.XPATH, element))
 
+def downloadWait():
+    dl_wait = True
+    while dl_wait:
+        sleep(1)
+        dl_wait = False
+        #go through the file name in the downloads folder
+        for fname in os.listdir(PATH_TO_DOWNLOADS):
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+    print("File Download Complete")
+
 def makeDosierReport(driver):
     folders = ["//a[text() = 'Financial Reporting']", "//div[text() = 'Shared Reports']", "//a[text() = 'Function Specific Content']", "//a[text() = 'Marketing']", "//a[text() = 'KGM - Concur Detail Report']", "//input[@value = 'Run Dossier']"]
     for folder in folders:
@@ -34,4 +45,11 @@ sleep(10)
 Next step:
 - find out how to download the data
 - note to self, angular is a mess to us selenium on
+
+theory:
+- respond when the elements are present
+- locate the more options menu for the concur detail report
+- select export
+- select data
+- wait for download to occur
 """                              
